e09cf6e85124d1885c458c57e89a4dc196dfeb6e
sirwior <34189374+sirwior@users.noreply.github.com>
2019-02-20 13:17:48 +0200

    Add files via upload

---
+++ Lab7Razor/Controllers/TeacherController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Lab7Razor.Models;
+
+namespace Lab7Razor.Controllers
+{
+    public class TeacherController : Controller
+    {
+        private Repository repo = new Repository();
+
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View(repo.Data);
+        }
+
+        [HttpPost]
+        public ActionResult Index(string fname, string lname, string subject, string description)
+        {
+            if(ModelState.IsValid)
+            {
+                repo.Data.Add(
+                        new Teacher
+                        {
+                            FirstName = fname,
+                            LastName = lname,
+                            Subject = subject,
+                            Description = description
+                        }
+                    );
+            }
+            return View(repo.Data);
+        }
+        
+        [HttpGet]
+        public ActionResult Delete(int id)
+        {
+            repo.Data.RemoveAt(id);
+            return View("Index", repo.Data);
+        }
+
+        [HttpGet]
+        public ActionResult Details(int id)
+        {
+            return View(repo.Data.ElementAt(id));
+        }
+    }
+}
---
+++ Lab7Razor/Models/Teacher.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Lab7Razor.Models
+{
+    public class Teacher
+    {
+        [Required(ErrorMessage = "Введите имя")]
+        public string FirstName { get; set; }
+        [Required(ErrorMessage = "Введите фамилию")]
+        public string LastName { get; set; }
+        [Required(ErrorMessage = "Введите предмет")]
+        public string Subject { get; set; }
+        public string Description { get; set; }
+    }
+}
